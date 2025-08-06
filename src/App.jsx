766acbbcbcf334d477d594f8db1ae00c87edcfed
KTH0920 <taehyun9482@gmail.com>
2025-08-06 13:45:38 +0900

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import './App.css'
-import Section from './components/Section'
+import "./App.css";
+import Button from "./components/Button";
+import Input from "./components/Input";
 
 function App() {
-
   return (
     <div>
-      <Section/>
       <h1>hello React</h1>
+      <Button text={"메일"} color={"red"} />
+      <Button text={"카페"} color={"blue"} />
+      <Button text={"블로그"} color={"orange"} />
+      <Button text={"기본값 설정"} />
+      <Button text={"자식요소 설정"} color={"pink"}>
+        <strong>중요</strong>
+      </Button>
+      <Input inputValue = {'hello react'} title={"input title"} placeholder={"입력하세요."} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
